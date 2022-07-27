2b731fbd3210e32ee2a0198427882a43d86c3769
Liliana <liliana.m.parra@gmail.com>
2022-07-27 15:30:11 -0400

    Changed user model to have self-referential many-to-many relationship

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,21 @@
 from app import db
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+user_to_user = db.Table("user_to_user", db.metadata,
+    db.Column("follower_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("friend_id", db.Integer, db.ForeignKey("user.id"), primary_key=True)
+)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String, nullable=False),
-    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    friends = relationship("User", remote_side=[id])
+    friends = db.relationship("User",
+                        secondary=user_to_user,
+                        primaryjoin=id==user_to_user.c.follower_id,
+                        secondaryjoin=id==user_to_user.c.friend_id,
+                        backref="followers"
+    )
 
 
 def self_to_dict(self):
